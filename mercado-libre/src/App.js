47609e352635e61a551d7e1e3f8601de58ea8826
Damian Cabrera <108781459+DamianRCabrera@users.noreply.github.com>
2023-01-04 20:28:40 -0300

    Initial stage of Mercadolibre project
    - Added shoppingCart logic to project
    - Added hook.
    - Added component to render shoppingCart.

---
+++ mercado-libre/src/App.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Axios from 'axios';
+import UseCarritoDeCompras from "./Hooks/useCarritoDeCompras/index.jsx";
+
+
+function App() {
+
+
+  const [json, setJson] = useState([]);
+  const [carrito,  agregarItem, quitarItem] = UseCarritoDeCompras();
+
+
+
+  useEffect(()=>{
+    Axios({
+      url: 'https://dummyjson.com/products'
+    })
+    .then((response)=>{
+      setJson(response.data.products)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  },[])
+
+  return (
+    <div className="App">
+      <ul>
+        {json.map((producto)=>(
+          <li>{producto.id} - {producto.title} <button onClick={()=>agregarItem(producto)} >Agregar</button> </li>    
+          ))}
+      </ul>
+      <ul>
+        {carrito.map((producto, index)=>(
+          <li>{producto.id} - {producto.title} <button onClick={()=>quitarItem(index)} >Quitar</button></li>    
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
